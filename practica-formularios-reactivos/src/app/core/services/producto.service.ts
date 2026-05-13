e4453b82416e4520fde9989997b8fd7113f34c08
Fabricio Alvarez <41123692+ea00d009@users.noreply.github.com>
2026-05-13 19:29:05 -0300

    Create ProductoService with postProducto method
    Added ProductoService for managing product operations.

--- practica-formularios-reactivos/src/app/core/services/producto.service.ts
+++ practica-formularios-reactivos/src/app/core/services/producto.service.ts
@@ -1,1 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ProductoCreateDTO, ProductoReadDTO } from '../models/producto.model';
 
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductoService {
+  // URL simulada para la práctica
+  private readonly API_URL = 'https://api.tu-servidor.com/productos';
+
+  constructor(private http: HttpClient) {}
+
+  postProducto(producto: ProductoCreateDTO): Observable<ProductoReadDTO> {
+    return this.http.post<ProductoReadDTO>(this.API_URL, producto);
+  }
+}
