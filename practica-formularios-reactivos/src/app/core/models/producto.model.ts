17513d12fc8763a06c7a0652c9b62690b2a6e468
Fabricio Alvarez <41123692+ea00d009@users.noreply.github.com>
2026-05-13 19:28:23 -0300

    Add DTO interfaces for Producto model

--- practica-formularios-reactivos/src/app/core/models/producto.model.ts
+++ practica-formularios-reactivos/src/app/core/models/producto.model.ts
@@ -1,1 +1,14 @@
+// DTO para enviar al servidor (Alta)
+export interface ProductoCreateDTO {
+  nombre: string;
+  categoriaId: number; 
+  precio: number;
+  descripcion?: string;
+  stock: number; 
+}
 
+// DTO para recibir del servidor (Lectura)
+export interface ProductoReadDTO extends ProductoCreateDTO {
+  id: number;
+  fechaCreacion: Date;
+}
