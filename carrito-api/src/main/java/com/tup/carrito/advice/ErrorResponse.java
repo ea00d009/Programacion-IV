f257642b1ab113672ec3bfab193f8f0d42491bb2
Fabricio Alvarez <41123692+ea00d009@users.noreply.github.com>
2026-05-18 20:02:26 -0300

    Update ErrorResponse.java

--- carrito-api/src/main/java/com/tup/carrito/advice/ErrorResponse.java
+++ carrito-api/src/main/java/com/tup/carrito/advice/ErrorResponse.java
@@ -1,1 +1,19 @@
+package com.tup.carrito.advice;
 
+import java.time.LocalDateTime;
+
+public class ErrorResponse {
+    private String mensaje;
+    private int estadoHttp;
+    private LocalDateTime timestamp;
+
+    public ErrorResponse(String mensaje, int estadoHttp, LocalDateTime timestamp) {
+        this.mensaje = mensaje;
+        this.estadoHttp = estadoHttp;
+        this.timestamp = timestamp;
+    }
+
+    public String getMensaje() { return mensaje; }
+    public int getEstadoHttp() { return estadoHttp; }
+    public LocalDateTime getTimestamp() { return timestamp; }
+}
