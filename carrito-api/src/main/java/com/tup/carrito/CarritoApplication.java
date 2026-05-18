bf62e85164f9f747f28d07fc8bbbe856b68dde51
Fabricio Alvarez <41123692+ea00d009@users.noreply.github.com>
2026-05-18 20:00:17 -0300

    Update CarritoApplication.java

--- carrito-api/src/main/java/com/tup/carrito/CarritoApplication.java
+++ carrito-api/src/main/java/com/tup/carrito/CarritoApplication.java
@@ -1,1 +1,9 @@
-
+package com.tup.carrito;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class CarritoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CarritoApplication.class, args);
+    }
+}
