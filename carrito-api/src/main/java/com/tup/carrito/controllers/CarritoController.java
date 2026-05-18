5d63b1e52a925903f78e90e4252f23770d0624ab
Fabricio Alvarez <41123692+ea00d009@users.noreply.github.com>
2026-05-18 20:05:28 -0300

    Update CarritoController.java

--- carrito-api/src/main/java/com/tup/carrito/controllers/CarritoController.java
+++ carrito-api/src/main/java/com/tup/carrito/controllers/CarritoController.java
@@ -1,1 +1,27 @@
+package com.tup.carrito.controllers;
 
+import com.tup.carrito.entities.Carrito;
+import com.tup.carrito.services.ICarritoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/carrito")
+public class CarritoController {
+
+    @Autowired
+    private ICarritoService carritoService;
+
+    @PostMapping("/{id}/productos")
+    public ResponseEntity<Carrito> agregarProductoAlCarrito(
+            @PathVariable Long id,
+            @RequestParam Long productoId,
+            @RequestParam Integer cantidad) {
+        
+        // El controlador queda completamente limpio. Todas las validaciones
+        // y cálculos ocurren en la Service Layer.
+        Carrito carrito = carritoService.agregarProducto(id, productoId, cantidad);
+        return ResponseEntity.ok(carrito);
+    }
+}
