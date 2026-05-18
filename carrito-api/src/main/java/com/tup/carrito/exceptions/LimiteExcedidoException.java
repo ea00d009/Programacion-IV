283ea8d40ca47b989447e75aef4d5c4d77765d59
Fabricio Alvarez <41123692+ea00d009@users.noreply.github.com>
2026-05-18 20:02:03 -0300

    Update LimiteExcedidoException.java

--- carrito-api/src/main/java/com/tup/carrito/exceptions/LimiteExcedidoException.java
+++ carrito-api/src/main/java/com/tup/carrito/exceptions/LimiteExcedidoException.java
@@ -1,1 +1,7 @@
+package com.tup.carrito.exceptions;
 
+public class LimiteExcedidoException extends RuntimeException {
+    public LimiteExcedidoException(String message) {
+        super(message);
+    }
+}
