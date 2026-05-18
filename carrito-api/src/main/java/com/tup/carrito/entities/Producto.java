f31eee750ba9605166277cc56b1c270ffcd24aaf
Fabricio Alvarez <41123692+ea00d009@users.noreply.github.com>
2026-05-18 20:00:51 -0300

    Update Producto.java

--- carrito-api/src/main/java/com/tup/carrito/entities/Producto.java
+++ carrito-api/src/main/java/com/tup/carrito/entities/Producto.java
@@ -1,1 +1,25 @@
+package com.tup.carrito.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+public class Producto {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private Double precio;
+    private Integer stockDisponible;
+    private boolean activo;
+
+    // Getters y Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public Double getPrecio() { return precio; }
+    public void setPrecio(Double precio) { this.precio = precio; }
+    public Integer getStockDisponible() { return stockDisponible; }
+    public void setStockDisponible(Integer stockDisponible) { this.stockDisponible = stockDisponible; }
+    public boolean isActivo() { return activo; }
+    public void setActivo(boolean activo) { this.activo = activo; }
+}
