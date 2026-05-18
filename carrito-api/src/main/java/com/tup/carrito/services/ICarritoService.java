d08b23768a32b1d28c9511c48c62faec269d9d5a
Fabricio Alvarez <41123692+ea00d009@users.noreply.github.com>
2026-05-18 20:03:47 -0300

    Update ICarritoService.java

--- carrito-api/src/main/java/com/tup/carrito/services/ICarritoService.java
+++ carrito-api/src/main/java/com/tup/carrito/services/ICarritoService.java
@@ -1,1 +1,6 @@
+package com.tup.carrito.services;
+import com.tup.carrito.entities.Carrito;
 
+public interface ICarritoService {
+    Carrito agregarProducto(Long carritoId, Long productoId, Integer cantidad);
+}
